956596a12bb23f4a3ae266601a3bde6bae5c250a
shipatulhasan <shipatulhasan328@gmail.com>
2023-11-14 21:17:17 +0600

    update formatting input

--- formating_output.cpp
+++ formating_output.cpp
@@ -1,7 +1,16 @@
-#include<iostream>;
+#include<iostream>
 #include<conio.h>
+#include<iomanip>
 using namespace std;
 int main (){
-
+float num1,num2,sum;
+cout<< "Enter 2 number: " ;
+cin >> num1 >> num2 ;
+ sum = num1 + num2;
+ cout<< showpoint;
+ cout<<fixed;
+ cout<<setprecision(2);
+cout<<sum;
+getch();
 
 }
